915c95c3d1f02bbca6c4cb7d329d24a53142ca9b
lvemilio <52930437+lvemilio@users.noreply.github.com>
2023-04-16 16:03:16 +0300

    Init Commit

---
+++ src/main/java/com/ipuniversityfinder/eurofins/EurofinsApplication.java
@@ -0,0 +1,13 @@
+package com.ipuniversityfinder.eurofins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurofinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurofinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipuniversityfinder/eurofins/EurofinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipuniversityfinder.eurofins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurofinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
